ea660b393f9cfb1c57dd9c74a3fb4600eaf7db2a
Jason Foreman <jason@threeve.org>
2020-05-22 00:46:26 -0500

    Add rudimentary sourcekit-lsp client support

--- src/extension.ts
+++ src/extension.ts
@@ -3,11 +3,20 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import * as vscode from "vscode";
+import { LanguageClient } from "vscode-languageclient";
 
 export function activate(context: vscode.ExtensionContext) {
 	let outputChannel = vscode.window.createOutputChannel("Swift");
 	context.subscriptions.push(outputChannel);
 	outputChannel.appendLine("Swift activated");
+
+	const languageClient = new LanguageClient(
+		"sourcekit-lsp",
+		"Swift Language Server",
+		{ command: "sourcekit-lsp" },
+		{ documentSelector: ["swift"] }
+	);
+	context.subscriptions.push(languageClient.start());
 }
 
 export function deactivate() {}
