88df82759cf4e61208524eca6d23cd4a30832aae
Jason Foreman <jason@threeve.org>
2020-05-22 21:36:34 -0500

    Locate sourcekit-lsp from path or Xcode toolchain

--- src/extension.ts
+++ src/extension.ts
@@ -4,19 +4,41 @@
 
 import * as vscode from "vscode";
 import { LanguageClient } from "vscode-languageclient";
+import * as which from "which";
 
 export function activate(context: vscode.ExtensionContext) {
 	let outputChannel = vscode.window.createOutputChannel("Swift");
 	context.subscriptions.push(outputChannel);
 	outputChannel.appendLine("Swift activated");
 
-	const languageClient = new LanguageClient(
-		"sourcekit-lsp",
-		"Swift Language Server",
-		{ command: "sourcekit-lsp" },
-		{ documentSelector: ["swift"] }
-	);
-	context.subscriptions.push(languageClient.start());
+	let lspPath = which.sync("sourcekit-lsp", { nothrow: true });
+	if (lspPath === null) {
+		outputChannel.appendLine(
+			"sourcekit-lsp not found in $PATH, checking for Xcode"
+		);
+		lspPath = which.sync("sourcekit-lsp", {
+			nothrow: true,
+			path:
+				"/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin",
+		});
+		if (lspPath === null) {
+			outputChannel.appendLine(
+				"sourcekit-lsp not found in Xcode.app, giving up"
+			);
+		}
+	}
+	if (lspPath !== null) {
+		outputChannel.appendLine(
+			`Found language server at "${lspPath}", launching now...`
+		);
+		const languageClient = new LanguageClient(
+			"sourcekit-lsp",
+			"Swift Language Server",
+			{ command: lspPath },
+			{ documentSelector: ["swift"] }
+		);
+		context.subscriptions.push(languageClient.start());
+	}
 }
 
 export function deactivate() {}
